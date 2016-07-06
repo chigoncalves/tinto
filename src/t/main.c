e131ee27519a40470efc1aa5a009cec14845c793
Edelcides Gonçalves <eatg75@gmail.com>
2016-07-06 20:04:26 +0100

    Configured CUnit so it does not show that anoying branding message.

--- src/t/main.c
+++ src/t/main.c
@@ -21,9 +21,10 @@ main (void) {
   test_string_addins ();
   test_dimension ();
 
-  CU_basic_set_mode(CU_BRM_VERBOSE);
-  CU_basic_run_tests();
-  CU_cleanup_registry();
+  CU_basic_set_mode (CU_BRM_SILENT);
+  CU_basic_run_tests ();
+  CU_basic_show_failures(CU_get_failure_list());
 
+  CU_cleanup_registry ();
   return EXIT_SUCCESS;
 }
