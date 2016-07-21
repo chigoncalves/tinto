a89e43bc38a394ad21c4e9f30fa358b8facd97f3
Edelcides Gonçalves <eatg75@gmail.com>
2016-07-21 19:00:16 +0100

    Updated *listfile*.

--- cmake/Utils.cmake
+++ cmake/Utils.cmake
@@ -1,39 +1,77 @@
+set (CMAKE_POSITION_INDEPENDENT_CODE ON)
+
+set (CMAKE_AUTOMOC ON)
+set (CMAKE_AUTORCC ON)
+set (CMAKE_AUTOUIC ON)
+set (CMAKE_RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin")
+
 if (NOT CMAKE_BUILD_TYPE)
   set (CMAKE_BUILD_TYPE "Release")
 endif ()
 
 if (CMAKE_BUILD_TYPE STREQUAL "Debug")
-  option (ENABLE_SAN "Build with for Google's sanitizers such as: asan, msan, tsan or lsan." on)
 
-  set (CMAKE_EXPORT_COMPILE_COMMANDS 1)
+  set (tinto_tests_SOURCES)
+
+  if (CMAKE_C_COMPILER_ID STREQUAL "Clang")
+    set (CLANG ON)
+    if (CMAKE_C_COMPILER_VERSION VERSION_GREATER "3.3.0")
+      set (COMPILER_SUPPORTS_SAN ON)
+    endif ()
+  elseif (CMAKE_C_COMPILER_ID STREQUAL "GNU")
+    set (GNU ON)
+    if (CMAKE_C_COMPILER_VERSION VERSION_GREATER "4.8.0")
+      set (COMPILER_SUPPORTS_SAN ON)
+    endif ()
+  endif ()
+
+  macro (list_stringfy varname)
+    list (REMOVE_DUPLICATES ${varname})
+    string (REPLACE ";" " " ${varname} "${${varname}}")
+  endmacro ()
+
+  set (CMAKE_WARN_DEPRECATED ON)
+  set (CMAKE_ERROR_DEPRECATED ON)
+  set (CMAKE_EXPORT_COMPILE_COMMANDS ON)
   set (DEBUG 1)
-  set (TINTO_DEVEL_MODE 1)
   unset (NDEBUG)
-  mark_as_advanced (
-    DEBUG
-    TINTO_DEVEL_MODE
-  )
 
-  # if (CMAKE_C_COMPILER_ID STREQUAL "Clang" OR CMAKE_C_COMPILER_ID STREQUAL "GNU")
-  #   set (CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -Wall -Wextra -std=c11 -pedantic")
+  option (ENABLE_SAN "Enable sanatizers." ON)
+  list (APPEND CMAKE_C_FLAGS_DEBUG -Wall -Wextra -Werror -std=c99
+                                   -pedantic)
+  if (COMPILER_SUPPORTS_SAN)
+    set (SAN_BLACKLIST_FILE "${CMAKE_SOURCE_DIR}/blacklists.txt")
+    list (APPEND CMAKE_C_FLAGS_DEBUG -fPIE )
+    list (APPEND CMAKE_EXE_LINKER_FLAGS_DEBUG -fno-omit-frame-pointer
+                                              -pie)
+    if (ENABLE_SAN)
+      string (TOLOWER ${ENABLE_SAN} ENABLE_SAN)
+      list (APPEND CMAKE_EXE_LINKER_FLAGS_DEBUG -pie)
+    endif ()
 
-  #   if (ENABLE_ASAN)
-  #     string (TOLOWER ${ENABLE_SAN} ENABLE_SAN)
-  #   else ()
-  #     set (ENABLE_SAN "off")
-  #   endif ()
+    if (CLANG)
+      list (APPEND CMAKE_C_FLAGS_DEBUG
+	           -fsanitize-blacklist="${SAN_BLACKLIST_FILE}")
+    endif ()
 
-  #   if (${ENABLE_SAN} STREQUAL "asan" OR ${ENABLE_SAN} STREQUAL "on")
-  #     set (CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -fsanitize=asan")
-  #   elseif (${ENABLE_SAN} STREQUAL "msan")
-  #     set (CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -fsanitize=memory")
-  #   elseif (${ENABLE_SAN} STREQUAL "tsan")
-  #     set (CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -fsanitize=thread")
-  #   elseif (${ENABLE_SAN} STREQUAL "lsan")
-  #     # set (CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS_DEBUG} -fsanitize=leak")
-  #     message (FATAL_ERROR "Lsan not enabled yet.")
-  #   endif ()
-  # endif ()
+    if (${ENABLE_SAN} STREQUAL "asan" OR ${ENABLE_SAN} STREQUAL "on")
+      list (APPEND CMAKE_EXE_LINKER_FLAGS_DEBUG -fsanitize=address
+	                                        -fsanitize=undefined)
+    elseif (${ENABLE_SAN} STREQUAL "msan")
+      list (APPEND CMAKE_EXE_LINKER_FLAGS_DEBUG -fsanitize=memory)
+    elseif (${ENABLE_SAN} STREQUAL "tsan")
+      list (APPEND CMAKE_EXE_LINKER_FLAGS_DEBUG -fsanitize=thread)
+    endif ()
+  endif ()
+
+  if (DEBUG)
+    add_custom_target (lint COMMAND oclint-json-compilation-database
+      $ENV{OCLINT_EXTRA_OPTIONS})
+  endif ()
+
+  list_stringfy (CMAKE_C_FLAGS_DEBUG)
+  list_stringfy (CMAKE_EXE_LINKER_FLAGS_DEBUG)
 endif ()
 
-set (SOURCES "")
+mark_as_advanced (COMILER_SUPPORTS_SAN
+                  CLANG GNU DEBUG)
